7b238e5b6af209b9c311a6419844564d6ae46137
Damian Gawin <gawin.damian@gmail.com>
2019-11-17 11:25:06 +0100

    update do fronta

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -1,15 +1,19 @@
 $(document).ready(function() {
 
     /* For the sticky navigation */
-    $('.js--wp-1').waypoint(function(direction) {
-        if (direction == "down") {
+    $('.js--topbar').waypoint(function(direction)
+    {
+        if (direction === "down")
+        {
             $('.main-nav').addClass('sticky');
-        } else {
-            $('.main-nav').removeClass('sticky');
         }
-    }, {
-        offset: '60px;'
-    });
+        else
+            {
+            $('.main-nav').removeClass('sticky');
+            }
+    },
+        {offset: '60px;'}
+    );
 
     /* Animations on scroll */
     $('.js--wp-1').waypoint(function(direction) {
