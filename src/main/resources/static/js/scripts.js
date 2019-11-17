c9c202140a570bc4a441d324f981889552ee3e24
Damian Gawin <gawin.damian@gmail.com>
2019-11-17 10:43:11 +0100

    update do fronta

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
 
     /* For the sticky navigation */
-    $('.js--opis').waypoint(function(direction) {
-        if (direction === "down") {
+    $('.js--wp-1').waypoint(function(direction) {
+        if (direction == "down") {
             $('.main-nav').addClass('sticky');
         } else {
             $('.main-nav').removeClass('sticky');
@@ -18,16 +18,5 @@ $(document).ready(function() {
         offset: '50%'
     });
 
-    $('.js--wp-2').waypoint(function(direction) {
-        $('.js--wp-2').addClass('animated fadeInUp');
-    }, {
-        offset: '50%'
-    });
-
-    $('.js--wp-3').waypoint(function(direction) {
-        $('.js--wp-3').addClass('animated fadeIn');
-    }, {
-        offset: '50%'
-    });
 
 });
